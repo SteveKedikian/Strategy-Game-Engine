565db510b842acf0a1424ad24f85071ab1793639
Steve Kedikian <105996903+SteveKedikian@users.noreply.github.com>
2023-06-12 19:28:32 +0400

    Delete headers directory

--- headers/game/auxiliary_functions.h
+++
@@ -1,7 +0,0 @@
-#include "../constants_and_enums/direction_enum.h"
-
-/*
-    After selecting anything and giving commands, returns the direction of the mouse from the object
-    Telling that click was from Object's left, right, up, or down
-*/
-Direction mouseRightClickDirectionFromObject(float delta_x, float delta_y);
